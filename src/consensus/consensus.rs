e4ae74052b59d1c484cca5db89fc7be99379de37
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-11-06 14:41:24 -0800

    WIP

--- src/consensus/consensus.rs
+++ src/consensus/consensus.rs
@@ -239,7 +239,7 @@ impl Consensus {
         timeout_config: TimeoutConfig,
         metrics: Metrics,
         tx_decision: Option<TxDecision<SnapchainValidatorContext>>,
-        message_rx: Arc<Mutex<Receiver<FCMessage>>>,
+        message_rx: Arc<Mutex<Receiver<FCMessage>>>, // TODO: can we do this without Arc/Mutex
     ) -> Self {
         Self {
             ctx,
