ed293c383143b122f82d862593d32430d947850d
Aditi Srinivasan <aditiharini@gmail.com>
2024-11-08 16:01:22 -0500

    wire block proposals

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use malachite_metrics::{Metrics, SharedRegistry};
+use snapchain::consensus::consensus::Decision;
 use std::collections::HashMap;
 use std::error::Error;
 use std::net::SocketAddr;
@@ -65,6 +66,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     );
 
     let (system_tx, mut system_rx) = mpsc::channel::<SystemMessage>(100);
+    let (decision_tx, mut decision_rx) = mpsc::channel::<Decision<SnapchainValidatorContext>>(100);
 
     let gossip_result = SnapchainGossip::create(keypair.clone(), addr, system_tx.clone());
     if let Err(e) = gossip_result {
@@ -132,22 +134,79 @@ async fn main() -> Result<(), Box<dyn Error>> {
         keypair.clone(),
         app_config.consensus.clone(),
         Some(app_config.rpc_address.clone()),
+<<<<<<< HEAD
+=======
+    );
+    let validator_set = SnapchainValidatorSet::new(vec![validator]);
+
+    let consensus_params = ConsensusParams {
+        start_height: Height::new(shard.shard_id(), 1),
+        initial_validator_set: validator_set,
+        address: validator_address.clone(),
+        threshold_params: Default::default(),
+    };
+
+    let ctx = SnapchainValidatorContext::new(keypair.clone());
+    let block_proposer = Arc::new(Mutex::new(BlockProposer::new(
+        validator_address.clone(),
+        shard.clone(),
+    )));
+    let shard_validator = ShardValidator::new(
+        validator_address.clone(),
+        Some(block_proposer.clone()),
+        None,
+    );
+    let consensus_actor = Consensus::spawn(
+        ctx,
+        shard,
+        consensus_params,
+        TimeoutConfig::default(),
+        metrics.clone(),
+        Some(decision_tx.clone()),
+>>>>>>> 5430457 (wire block proposals)
         gossip_tx.clone(),
     )
     .await;
 
     tokio::spawn(async move {
         let block_proposer = block_proposer.clone();
-        while let Some((_height, _round, shard_hash)) = decision_rx.recv().await {
+        while let Some((decision_height, _round, shard_hash)) = decision_rx.recv().await {
             let block_proposer = block_proposer.lock().await;
             let block = block_proposer.get_proposed_value(&shard_hash);
+            let mut blocks_by_shard = blocks_by_shard.lock().await;
             match block {
-                Some(block) => match put_block(&rocksdb, block) {
-                    Err(err) => {
-                        error!("Error writing block to db {:#?}", err)
+                Some(block) => {
+                    // TODO(aditi): Substitute out with persistence
+                    let shard_index = shard_hash.shard_index;
+                    // Maintain sorted lists of blocks in blocks_by_shard
+                    match blocks_by_shard.get_mut(&shard_index) {
+                        None => {
+                            blocks_by_shard.insert(shard_index, vec![block.clone()]);
+                        }
+                        Some(blocks) => match blocks.binary_search_by(|block| match &block.header {
+                            None =>
+                            // This is unexpected, keep at end of list
+                            {
+                                std::cmp::Ordering::Greater
+                            }
+                            Some(header) => match &header.height {
+                                None =>
+                                // This is unexpected, keep at end of list
+                                {
+                                    std::cmp::Ordering::Greater
+                                }
+                                Some(height) => {
+                                    height.block_number.cmp(&decision_height.block_number)
+                                }
+                            },
+                        }) {
+                            Err(pos) => {
+                                blocks.insert(pos, block.clone());
+                            }
+                            Ok(_) => {}
+                        },
                     }
-                    Ok(()) => {}
-                },
+                }
                 None => {
                     error!("Missing block for proposal. Shard hash: {:#?}", shard_hash);
                 }
