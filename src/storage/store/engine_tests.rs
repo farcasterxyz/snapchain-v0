8dc1aa540dffb7912af551688520d754a2185ace
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-11-18 11:48:26 -0800

    More testing

--- src/storage/store/engine_tests.rs
+++ src/storage/store/engine_tests.rs
@@ -15,12 +15,24 @@ mod tests {
         ShardEngine::new(1, shard_store)
     }
 
+    fn from_hex(s: &str) -> Vec<u8> {
+        hex::decode(s).unwrap()
+    }
+
+    fn to_hex(b: &[u8]) -> String {
+        hex::encode(b)
+    }
+
     #[test]
     fn test_engine_one() {
         let mut engine = new_engine();
         let state_change = engine.propose_state_change(1);
         assert_eq!(1, state_change.shard_id);
-        assert_eq!(state_change.transactions.len(), 1); // TODO: will change
-        state_change.transactions[]
+        assert_eq!(state_change.transactions.len(), 1);
+        assert_eq!(0, state_change.transactions[0].user_messages.len());
+        assert_eq!(
+            "237b11d0dd9e78994ef2f141c7f170d48bb51d34",
+            to_hex(&state_change.new_state_root)
+        );
     }
 }
