d4e099c7039e7ced85be9a5b5e22439fb6d9c016
Aditi Srinivasan <aditiharini@gmail.com>
2024-11-21 14:53:17 -0500

    compiles

--- src/network/gossip.rs
+++ src/network/gossip.rs
@@ -50,7 +50,7 @@ impl Config {
     pub fn bootstrap_addrs(&self) -> Vec<String> {
         self.bootstrap_peers
             .split(',')
-            .map(|s| s.to_string().trim())
+            .map(|s| s.trim().to_string())
             .collect()
     }
 }
